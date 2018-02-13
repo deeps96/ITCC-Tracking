1a85897205a014d9469da4ef72a5c9735d751099
deeps96 <leonardo.huebscher.business@gmail.com>
2018-02-13 18:29:44 +0100

    Removed commented code. #12

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,19 +2,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
 
 export const ROUTE_CONFIG: Routes = [
-  /*{
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
-    path: '**',
-    redirectTo: 'home'
-  }*/
   {
     path: 'login',
     component: LoginComponent
