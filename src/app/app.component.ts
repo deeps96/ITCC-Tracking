ab71b0b4d4f00738e6c309144cb9c0ecfd0b9761
deeps96 <leonardo.huebscher.business@gmail.com>
2018-02-13 22:50:07 +0100

    Added link to login to index page. Implemented logic to only show the login button, if user is not on the login page and not logged in already. #12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import {Router} from "@angular/router";
+import {NavigationStart, Router} from "@angular/router";
+import 'rxjs/add/operator/filter';
+import {AuthorizationResponse} from "./authentication";
+import {AuthenticationService} from "./authentication.service";
+
 
 @Component({
   selector: 'app-root',
@@ -8,5 +12,22 @@ import {Router} from "@angular/router";
 })
 export class AppComponent {
 
-  constructor(private router: Router) { }
+  public showLoginButton ;
+
+  constructor(private router: Router, private authenticationService: AuthenticationService) {
+    router.events
+      .filter(event => event instanceof NavigationStart)
+      .subscribe((event: NavigationStart) => {
+        this.showLoginButton = !this.isLoginPage(event) && !this.isLoggedIn();
+      });
+  }
+
+  //actions
+  private isLoginPage(event: NavigationStart): boolean {
+    return event.url == '/login';
+  }
+
+  private isLoggedIn(): boolean {
+    return this.authenticationService.isAuthorized();
+  }
 }
