246adafbff7a218259075f5848b457f05ab664a9
deeps96 <leonardo.huebscher.business@gmail.com>
2018-02-13 16:39:36 +0100

    Implemented authorization logic. Added Guava for SHA-256 calculation. #8

--- src/main/java/de/deeps/tracking/controller/AuthorizationController.java
+++ src/main/java/de/deeps/tracking/controller/AuthorizationController.java
@@ -1,14 +1,11 @@
 package de.deeps.tracking.controller;
 
 import de.deeps.tracking.dto.AuthorizationResponse;
-import de.deeps.tracking.repository.AuthorizationRepository;
 import de.deeps.tracking.service.AuthorizationService;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,11 +23,14 @@ public class AuthorizationController {
     }
 
     @RequestMapping(value = "/authorize", method = RequestMethod.GET, produces = "application/json")
-    public AuthorizationResponse shopIDToUrl(@RequestParam(value="email") String email, @RequestParam(value="password")
+    public AuthorizationResponse authorize(@RequestParam(value="email") String email, @RequestParam(value="password")
             String password) {
-        boolean isAuthorized = getService().isAuthorized(email, password);
-
-        return new AuthorizationResponse();
+        AuthorizationResponse response = new AuthorizationResponse();
+        response.setAuthorized(getService().isAuthorized(email, password));
+        if (response.isAuthorized()) {
+            response.setAuthorizationToken(getService().generateAuthenticationToken(email));
+        }
+        return response;
     }
 
 }
--- src/main/java/de/deeps/tracking/service/AuthorizationService.java
+++ src/main/java/de/deeps/tracking/service/AuthorizationService.java
@@ -1,25 +1,63 @@
 package de.deeps.tracking.service;
 
-import de.deeps.tracking.model.AuthorizationEntry;
-import de.deeps.tracking.repository.AuthorizationRepository;
+import com.google.common.hash.Hashing;
+import de.deeps.tracking.model.dbobjects.AuthorizationEntry;
+import de.deeps.tracking.model.dbobjects.AuthorizationToken;
+import de.deeps.tracking.repository.AuthorizationEntriesRepository;
+import de.deeps.tracking.repository.AuthorizationTokensRepository;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
 @Service
-@Getter(AccessLevel.PRIVATE) @Setter(AccessLevel.PRIVATE)
+@Getter(AccessLevel.PRIVATE)
+@Setter(AccessLevel.PRIVATE)
 public class AuthorizationService {
 
-    private AuthorizationRepository authorizationRepository;
+    private AuthorizationEntriesRepository authorizationEntriesRepository;
+    private AuthorizationTokensRepository authorizationTokensRepository;
 
+    //initialization
     @Autowired
-    public AuthorizationService(AuthorizationRepository authorizationRepository) {
-        setAuthorizationRepository(authorizationRepository);
+    public AuthorizationService(AuthorizationEntriesRepository authorizationEntriesRepository,
+                                AuthorizationTokensRepository authorizationTokensRepository) {
+        setAuthorizationEntriesRepository(authorizationEntriesRepository);
+        setAuthorizationTokensRepository(authorizationTokensRepository);
     }
 
+    //convenience
     public boolean isAuthorized(String email, String password) {
-        return false;
+        AuthorizationEntry entry = getAuthorizationEntriesRepository().findByEmail(email);
+        return isAuthorized(entry, password);
     }
+
+    public String generateAuthenticationToken(String email) {
+        String token = generateNewToken();
+        storeAuthentificationToken(email, token);
+        return token;
+    }
+
+    //actions
+    private String hashPassword(String rawPassword) {
+        return Hashing.sha256().hashString(rawPassword, StandardCharsets.UTF_8).toString();
+    }
+
+    private String generateNewToken() {
+        return UUID.randomUUID().toString();
+    }
+
+    private void storeAuthentificationToken(String email, String token) {
+        getAuthorizationTokensRepository().save(new AuthorizationToken(email, token));
+    }
+
+    //conditionals
+    private boolean isAuthorized(AuthorizationEntry entry, String password) {
+        return entry != null && entry.getHashedPassword().equals(hashPassword(password));
+    }
+
 }
