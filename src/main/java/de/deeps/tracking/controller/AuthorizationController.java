afc0b22f8abc4f300a693c8d44f0bef8bba26d2d
deeps96 <leonardo.huebscher.business@gmail.com>
2018-02-13 23:10:49 +0100

    Added CORS to controller.

--- src/main/java/de/deeps/tracking/controller/AuthorizationController.java
+++ src/main/java/de/deeps/tracking/controller/AuthorizationController.java
@@ -6,12 +6,10 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @Getter(AccessLevel.PRIVATE) @Setter(AccessLevel.PRIVATE)
 public class AuthorizationController {
 
