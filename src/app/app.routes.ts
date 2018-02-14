6c4535f1a853d1bbf10abd43d95674153022b5a3
deeps96 <leonardo.huebscher.business@gmail.com>
2018-02-14 19:26:43 +0100

    Created background for HomeComponent. #16

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
+import {HomeComponent} from "./home/home.component";
 
 export const ROUTE_CONFIG: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: '',
+    component: HomeComponent
   }
 ];
 
