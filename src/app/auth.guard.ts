083c152d235da4fb021add0935ac3627d0395004
deeps96 <leonardo.huebscher.business@gmail.com>
2018-02-16 21:12:28 +0100

    Fixed bug in auth guard.

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
 import { Observable } from 'rxjs/Observable';
-import {AuthenticationService} from "./authorization.service";
+import {AuthorizationService} from "./authorization.service";
 import {ROUTER_CONFIG} from "../assets/config";
 import {RouterConfig} from "./config";
 import {forEach} from "@angular/router/src/utils/collection";
@@ -11,7 +11,7 @@ export class AuthGuard implements CanActivate {
 
   private routerConfig: RouterConfig = ROUTER_CONFIG;
 
-  constructor(private router: Router, private authenticationService: AuthenticationService) {}
+  constructor(private router: Router, private authenticationService: AuthorizationService) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
@@ -20,21 +20,32 @@ export class AuthGuard implements CanActivate {
     let route: string = state.url;
     let allow: boolean = false;
     if (this.isAnonymUser()) {
-      allow = this.routeMatchesRegexFromList(this.routerConfig.allowedUserRoutes, route);
-    } else {
-      this.authenticationService.isAdmin().subscribe(response => {
-          if (response) { allow = this.routeMatchesRegexFromList(this.routerConfig.allowedAdminRoutes, route); }
-        });
-      if (!allow) {
-        this.authenticationService.isStaff().subscribe(response => {
-          if (response) { allow = this.routeMatchesRegexFromList(this.routerConfig.allowedStaffRoutes, route); }
+      return this.canAnonymActivate(route);
+    }
+    return this.authenticationService.isAdmin().map(response => {
+      if (response) {
+        return this.routeMatchesRegexFromList(this.routerConfig.allowedAdminRoutes, route);
+      } else {
+        return this.authenticationService.isStaff().map(response => {
+          response && this.routeMatchesRegexFromList(this.routerConfig.allowedStaffRoutes, route);
+        }).map(allowed => {
+          if (!allowed) {
+            this.router.navigate(['']);
+          }
+          return allowed;
         });
       }
+    });
+
+  }
+
+  //actions
+  private canAnonymActivate(route: string): boolean {
+    if (!this.routeMatchesRegexFromList(this.routerConfig.allowedUserRoutes, route)) {
+      this.router.navigate(['/login'], { queryParams: { returnUrl: route }});
+      return false;
     }
-    if (!allow) {
-      this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
-    }
-    return allow;
+    return true;
   }
 
   private routeMatchesRegexFromList(allowedRoutes: string[], route: string): boolean {
--- src/assets/config.ts
+++ src/assets/config.ts
@@ -13,4 +13,4 @@ export const ROUTER_CONFIG: RouterConfig = {
     '^$',
     '^/login'
   ]
-}
+};
