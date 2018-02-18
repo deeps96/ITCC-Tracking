619f9274b83e5929f242df797c853113977c7592
deeps96 <leonardo.huebscher.business@gmail.com>
2018-02-18 13:56:42 +0100

    Created custom markers for GoogleMaps Component. #15

--- src/app/parcel-details/parcel-details.component.ts
+++ src/app/parcel-details/parcel-details.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute, ParamMap} from "@angular/router";
 import {Parcel} from "../parcel-management";
 import {ParcelManagementService} from "../parcel-management.service";
@@ -16,6 +16,7 @@ export class ParcelDetailsComponent implements OnInit {
   @ViewChild('map')
   private mapComponent: any;
 
+  public addresses: string[];
   private center = {lat: 52.040055, lng: 8.540920};
   public mapOptions: MapOptions = {
     center: this.center,
@@ -24,200 +25,139 @@ export class ParcelDetailsComponent implements OnInit {
     zoom: 13,
     disableDefaultUI: true,
     zoomControl: true,
-    styles: [
-      {
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#f5f5f5"
-          }
-        ]
-      },
-      {
-        "elementType": "labels.icon",
-        "stylers": [
-          {
-            "visibility": "off"
-          }
-        ]
-      },
-      {
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#616161"
-          }
-        ]
-      },
-      {
-        "elementType": "labels.text.stroke",
-        "stylers": [
-          {
-            "color": "#f5f5f5"
-          }
-        ]
-      },
-      {
-        "featureType": "administrative",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "visibility": "off"
-          }
-        ]
-      },
-      {
-        "featureType": "administrative.land_parcel",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#bdbdbd"
-          }
-        ]
-      },
-      {
-        "featureType": "poi",
-        "stylers": [
-          {
-            "visibility": "off"
-          }
-        ]
-      },
-      {
-        "featureType": "poi",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#eeeeee"
-          }
-        ]
-      },
-      {
-        "featureType": "poi",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#757575"
-          }
-        ]
-      },
-      {
-        "featureType": "poi.park",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#e5e5e5"
-          }
-        ]
-      },
-      {
-        "featureType": "poi.park",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#9e9e9e"
-          }
-        ]
-      },
-      {
-        "featureType": "road",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#ffffff"
-          }
-        ]
-      },
-      {
-        "featureType": "road",
-        "elementType": "labels.icon",
-        "stylers": [
-          {
-            "visibility": "off"
-          }
-        ]
-      },
-      {
-        "featureType": "road.arterial",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#757575"
-          }
-        ]
-      },
-      {
-        "featureType": "road.highway",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#dadada"
-          }
-        ]
-      },
-      {
-        "featureType": "road.highway",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#616161"
-          }
-        ]
-      },
-      {
-        "featureType": "road.local",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#9e9e9e"
-          }
-        ]
-      },
-      {
-        "featureType": "transit",
-        "stylers": [
-          {
-            "visibility": "off"
-          }
-        ]
-      },
-      {
-        "featureType": "transit.line",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#e5e5e5"
-          }
-        ]
-      },
-      {
-        "featureType": "transit.station",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#eeeeee"
-          }
-        ]
-      },
-      {
-        "featureType": "water",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#c9c9c9"
-          }
-        ]
-      },
-      {
-        "featureType": "water",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#9e9e9e"
-          }
-        ]
+    styles: [{
+      "featureType": "all",
+      "elementType": "geometry.fill",
+      "stylers": [{
+        "weight": 2.00
       }
+      ]
+    }, {
+      "featureType": "all",
+      "elementType": "geometry.stroke",
+      "stylers": [{
+        "color": "#9c9c9c"
+      }
+      ]
+    }, {
+      "featureType": "all",
+      "elementType": "labels.text",
+      "stylers": [{
+        "visibility": "on"
+      }
+      ]
+    }, {
+      "featureType": "landscape",
+      "elementType": "all",
+      "stylers": [{
+        "color": "#f2f2f2"
+      }
+      ]
+    }, {
+      "featureType": "landscape",
+      "elementType": "geometry.fill",
+      "stylers": [{
+        "color": "#ffffff"
+      }
+      ]
+    }, {
+      "featureType": "landscape.man_made",
+      "elementType": "geometry.fill",
+      "stylers": [{
+        "color": "#ffffff"
+      }
+      ]
+    }, {
+      "featureType": "poi",
+      "elementType": "all",
+      "stylers": [{
+        "visibility": "off"
+      }
+      ]
+    }, {
+      "featureType": "road",
+      "elementType": "all",
+      "stylers": [{
+        "saturation": -100
+      }, {
+        "lightness": 45
+      }
+      ]
+    }, {
+      "featureType": "road",
+      "elementType": "geometry.fill",
+      "stylers": [{
+        "color": "#eeeeee"
+      }
+      ]
+    }, {
+      "featureType": "road",
+      "elementType": "labels.text.fill",
+      "stylers": [{
+        "color": "#7b7b7b"
+      }
+      ]
+    }, {
+      "featureType": "road",
+      "elementType": "labels.text.stroke",
+      "stylers": [{
+        "color": "#ffffff"
+      }
+      ]
+    }, {
+      "featureType": "road.highway",
+      "elementType": "all",
+      "stylers": [{
+        "visibility": "simplified"
+      }
+      ]
+    }, {
+      "featureType": "road.arterial",
+      "elementType": "labels.icon",
+      "stylers": [{
+        "visibility": "off"
+      }
+      ]
+    }, {
+      "featureType": "transit",
+      "elementType": "all",
+      "stylers": [{
+        "visibility": "off"
+      }
+      ]
+    }, {
+      "featureType": "water",
+      "elementType": "all",
+      "stylers": [{
+        "color": "#46bcec"
+      }, {
+        "visibility": "on"
+      }
+      ]
+    }, {
+      "featureType": "water",
+      "elementType": "geometry.fill",
+      "stylers": [{
+        "color": "#c8d7d4"
+      }
+      ]
+    }, {
+      "featureType": "water",
+      "elementType": "labels.text.fill",
+      "stylers": [{
+        "color": "#070707"
+      }
+      ]
+    }, {
+      "featureType": "water",
+      "elementType": "labels.text.stroke",
+      "stylers": [{
+        "color": "#ffffff"
+      }
+      ]
+    }
     ]
+
   };
   public parcel: Parcel;
   public positions: LatLngLiteral[];
@@ -242,8 +182,8 @@ export class ParcelDetailsComponent implements OnInit {
 
   private loadPositions(): void {
     this.positions = [];
-    let addresses: string[] = this.buildAddressArrayForStations();
-    addresses.forEach(address => {
+    this.buildAddressArrayForStations();
+    this.addresses.forEach(address => {
       this.mapComponent.geoCoder.geocode({address: address})
         .map(response => response[0])
         .subscribe(response =>
@@ -251,13 +191,12 @@ export class ParcelDetailsComponent implements OnInit {
     });
   }
 
-  private buildAddressArrayForStations(): string[] {
-    let addresses: string[] = [];
-    addresses.push(this.parcel.departure.road + ', ' + this.parcel.departure.city.name + ' ' + this.parcel.departure.country);
+  private buildAddressArrayForStations(): void {
+    this.addresses = [];
+    this.addresses.push(this.parcel.departure.road + ', ' + this.parcel.departure.city.name + ' ' + this.parcel.departure.country);
     this.parcel.stations.forEach(station =>
-      addresses.push(station.location.road + ', ' + station.location.city.name + ' ' + station.location.country));
-    addresses.push(this.parcel.destination.road + ', ' + this.parcel.destination.city.name + ' ' + this.parcel.destination.country);
-    return addresses;
+      this.addresses.push(station.location.road + ', ' + station.location.city.name + ' ' + station.location.country));
+    this.addresses.push(this.parcel.destination.road + ', ' + this.parcel.destination.city.name + ' ' + this.parcel.destination.country);
   }
 
 }
